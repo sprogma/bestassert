9fe4c6609b770981ef12bb09943bc46dc3ae4b82
sprogma <mux-228@yandex.ru>
2025-08-24 20:23:47 +0300

    upgrade compilation and .sh script generator

--- build.ps1
+++ build.ps1
@@ -2,8 +2,17 @@ param(
     [string[]]$CompilerFlags
 )
 
+
+function Run-Command
+{
+    Write-Host ($MyInvocation.UnboundArguments)
+    $cmd = @($MyInvocation.UnboundArguments[1..$MyInvocation.UnboundArguments.Count])
+    &$MyInvocation.UnboundArguments[0] @cmd
+}
+
+
 # build game :)
-gcc game/game.c -o bestassert_game$($IsWindows ?'.exe':'') 2>$null
+Run-Command -- gcc game/game.c -o bestassert_game$($IsWindows ?'.exe':'') 2>$null
 
 # build assert
 $build = @()
@@ -26,27 +35,21 @@ $platform = ($IsWindows ?"win":"lin")
 
 
 
-Write-Host -- gcc $F -O0 -g -c any/bestassert.c -o any/bestassert.o
-gcc $F -O0 -g -c any/bestassert.c -o any/bestassert.o
+Run-Command -- gcc $F -O0 -g -c any/bestassert.c -o any/bestassert.o
 
 $build += "any/bestassert.o"
 
-Write-Host -- gcc $F -O0 -g -c $platform/bestassert.c -o $platform/bestassert.o
-gcc $F -O0 -g -c $platform/bestassert.c -o $platform/bestassert.o
+Run-Command -- gcc $F -O0 -g -c $platform/bestassert.c -o $platform/bestassert.o
 
 $build += "$platform/bestassert.o"
 
-Write-Host -- gcc $F -O0 -g -c $platform/signals.c -o $platform/signals.o
-gcc $F -O0 -g -c $platform/signals.c -o $platform/signals.o
+Run-Command -- gcc $F -O0 -g -c $platform/signals.c -o $platform/signals.o
 
 $build += "$platform/signals.o"
 
-
-Write-Host -- gcc $L $build -shared -o "$($IsWindows ?'':'lib')bestassert.$($IsWindows ?'dll':'so')"
-gcc $L $build -shared "-Wl,--out-implib,libbestassert.lib" -o "$($IsWindows ?'':'lib')bestassert.$($IsWindows ?'dll':'so')"
+Run-Command -- gcc $L $build -shared "-Wl,--out-implib,libbestassert.lib" -o "$($IsWindows ?'':'lib')bestassert.$($IsWindows ?'dll':'so')"
 
 # if (!$IsWindows)
 # {
-#     Write-Host -- ar rcs libmyfuncs.a foo.o bar.o
-#     ar rcs libbestassert.a $build
+#     Run-Command -- ar rcs libbestassert.a $build
 # }
--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
+gcc game/game.c -o bestassert_game
 gcc -Wall -Wextra -DCREATE_NEW_CONSOLE_FOR_GDB -DADD_SIGNAL_HANDLERS -DKILL_GDB -Iinclude -fPIC -fpic -O0 -g -c any/bestassert.c -o any/bestassert.o
 gcc -Wall -Wextra -DCREATE_NEW_CONSOLE_FOR_GDB -DADD_SIGNAL_HANDLERS -DKILL_GDB -Iinclude -fPIC -fpic -O0 -g -c lin/bestassert.c -o lin/bestassert.o
 gcc -Wall -Wextra -DCREATE_NEW_CONSOLE_FOR_GDB -DADD_SIGNAL_HANDLERS -DKILL_GDB -Iinclude -fPIC -fpic -O0 -g -c lin/signals.c -o lin/signals.o
-gcc -fPIC -fpic any/bestassert.o lin/bestassert.o lin/signals.o -shared -o libbestassert.so
+gcc -fPIC -fpic any/bestassert.o lin/bestassert.o lin/signals.o -shared -Wl,--out-implib,libbestassert.lib -o libbestassert.so
